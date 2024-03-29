f5f6fed3720bdd7b35b8c50d985e047333c1e557
Rana Muneeb <rana-muneeb.hassan@s2022.tu-chemnitz.de>
2024-03-29 20:19:21 +0100

    Old

--- src/main/java/spring/course/data/UserEntity.java
+++ src/main/java/spring/course/data/UserEntity.java
@@ -1,2 +1,11 @@
-package spring.course.data;public class UserEntity {
+package spring.course.data;
+
+import jakarta.persistence.Entity;
+
+import java.io.Serializable;
+@Entity
+public class UserEntity implements Serializable {
+    private String lastName;
+    private Integer memberId;
+    private String firstName;
 }
